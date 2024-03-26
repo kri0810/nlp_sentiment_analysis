e4295de1eb4b94ad8695e9d0cdd1d327a24f8d42
krisha <krisha@htree.plus>
2024-03-26 18:28:02 +0530

    final utils

--- streamlit_frontend/utils.py
+++ streamlit_frontend/utils.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import nltk
 import string
+from fastapi import FastAPI, HTTPException
 import re
 from nltk.corpus import stopwords
 import nltk
@@ -101,3 +102,44 @@ def load_models():
         return loaded_model_a, loaded_model_b, loaded_model_c, tfidf_vectorizer
     except Exception as e:
         raise Exception(status_code=500, detail=f"Error occurred while loading models: {e}")
+
+def make_predictions(unseen_tweet, tfidf_vectorizer, loaded_model_a, loaded_model_b, loaded_model_c):
+    try:
+        # Preprocess the unseen tweet
+        tweet_processor = TweetProcessor()
+        preprocessed_tweet = tweet_processor.process_tweets(pd.DataFrame({'tweet': [unseen_tweet]}))['stemmed_tweet'].iloc[0]
+
+        # Vectorize the preprocessed tweet using the TF-IDF vectorizer
+        X_unseen = tfidf_vectorizer.transform([preprocessed_tweet])
+
+        # Empty list to store prediction results
+        predictions = []
+
+        # Make predictions using Model A
+        prediction_a = loaded_model_a.predict(X_unseen)
+        if prediction_a == 0:
+            predictions.append("Not offensive")
+        else:
+            predictions.append("Offensive")
+
+            # Make predictions using Model B
+            prediction_b = loaded_model_b.predict(X_unseen)
+            
+            # Check if the tweet is targeted
+            if prediction_b == 0:
+                predictions.append("Untargeted")
+            else:
+                predictions.append("Targeted")
+
+                # Make predictions using Model C
+                prediction_c = loaded_model_c.predict(X_unseen)
+                if prediction_c == 0:
+                    predictions.append('Group') 
+                elif prediction_c == 1:
+                    predictions.append('Individual') 
+                else:
+                    predictions.append('Others') 
+
+        return predictions
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error occurred while making predictions: {e}")
