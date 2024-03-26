0bd6b6c1c9ec03e54d486b7d3143093ef6cfce9f
krisha <krisha@htree.plus>
2024-03-26 18:25:50 +0530

    final frontend

--- streamlit_frontend/app7.py
+++ streamlit_frontend/app7.py
@@ -1,63 +1,8 @@
+#importing libraries and dependencies
 import streamlit as st
 import pickle
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from utils import TweetProcessor
-
-def load_models():
-    try:
-        with open('random_forest_model_a.pkl', 'rb') as file:
-            loaded_model_a = pickle.load(file)
-        
-        with open('random_forest_model_b.pkl', 'rb') as file:
-            loaded_model_b = pickle.load(file)
-        
-        with open('random_forest_model_c.pkl', 'rb') as file:
-            loaded_model_c = pickle.load(file)
-        
-        return loaded_model_a, loaded_model_b, loaded_model_c
-    except Exception as e:
-        st.error(f"Error occurred while loading models: {e}")
-        return None, None, None
-
-# Function to make predictions
-def make_predictions(unseen_tweet, tfidf_vectorizer, loaded_model_a, loaded_model_b, loaded_model_c):
-    try:
-        # Vectorize the unseen data using the TF-IDF vectorizer
-        X_unseen = tfidf_vectorizer.transform([unseen_tweet])
-
-        # Empty list to store prediction results
-        predictions = []
-
-        # Make predictions using Model A
-        prediction_a = loaded_model_a.predict(X_unseen)
-        if prediction_a == 0:
-            predictions.append("Not offensive")
-        else:
-            predictions.append("Offensive")
-
-            # Make predictions using Model B
-            prediction_b = loaded_model_b.predict(X_unseen)
-            
-            # Check if the tweet is targeted
-            if prediction_b == 0:
-                predictions.append("Untargeted")
-            else:
-                predictions.append("Targeted")
-
-                # Make predictions using Model C
-                prediction_c = loaded_model_c.predict(X_unseen)
-                if prediction_c == 0:
-                    predictions.append('Group') 
-                elif prediction_c == 1:
-                    predictions.append('Individual') 
-                else:
-                    predictions.append('Others') 
-
-        return predictions
-    except Exception as e:
-        st.error(f"Error occurred while making predictions: {e}")
-        return []
+from utils import load_models, TweetProcessor,  make_predictions
 
 # Streamlit UI
 def main():
@@ -76,27 +21,20 @@ def main():
         st.error(f"Error occurred while preprocessing tweet: {e}")
         preprocessed_tweet = ''
 
-    # Loading the trained models
-    loaded_model_a, loaded_model_b, loaded_model_c = load_models()
+    # Loading the trained models and TF-IDF vectorizer
+    loaded_model_a, loaded_model_b, loaded_model_c, tfidf_vectorizer = load_models()
 
     # Button to make predictions
     if st.button('Make Predictions'):
-        # Loading the TF-IDF vectorizer
-        try:
-            with open('tfidf_vectorizer.pkl', 'rb') as file:
-                tfidf_vectorizer = pickle.load(file)
-        except Exception as e:
-            st.error(f"Error occurred while loading TF-IDF vectorizer: {e}")
-            tfidf_vectorizer = None
-        
-        # Making predictions
-        if tfidf_vectorizer is not None and all((loaded_model_a, loaded_model_b, loaded_model_c)):
+        # Making predictions using the function from utils
+        if all((loaded_model_a, loaded_model_b, loaded_model_c)):
             predictions = make_predictions(preprocessed_tweet, tfidf_vectorizer, loaded_model_a, loaded_model_b, loaded_model_c)
             
             # Display predictions
             if predictions:
                 st.subheader('Predictions:')
                 prediction_str = ' '.join(predictions)
-                st.write(prediction_str)
+                st.write("The tweet is " ,prediction_str)
+
 if __name__ == '__main__':
     main()
