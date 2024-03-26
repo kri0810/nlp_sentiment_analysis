87d9c89d3d0034e1a2c6829e920d7423624964bd
krisha <krisha@htree.plus>
2024-03-26 18:26:56 +0530

    final backend

--- streamlit_frontend/main2.py
+++ streamlit_frontend/main2.py
@@ -1,59 +1,25 @@
+#importing libraries and dependencies
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from streamlit_frontend.utils import load_models, TweetProcessor
+from streamlit_frontend.utils import load_models, TweetProcessor, make_predictions
 
+#Creating an instance of the FastAPI class and assigning it to the variable app
 app = FastAPI()
 
+#structure of the request payload for the predict end point
 class TweetRequest(BaseModel):
     tweet: str
 
-
-def make_predictions(unseen_tweet, tfidf_vectorizer, loaded_model_a, loaded_model_b, loaded_model_c):
-    try:
-        # Preprocess the unseen tweet
-        tweet_processor = TweetProcessor()
-        preprocessed_tweet = tweet_processor.process_tweets(pd.DataFrame({'tweet': [unseen_tweet]}))['stemmed_tweet'].iloc[0]
-
-        # Vectorize the preprocessed tweet using the TF-IDF vectorizer
-        X_unseen = tfidf_vectorizer.transform([preprocessed_tweet])
-
-        # Empty list to store prediction results
-        predictions = []
-
-        # Make predictions using Model A
-        prediction_a = loaded_model_a.predict(X_unseen)
-        if prediction_a == 0:
-            predictions.append("Not offensive")
-        else:
-            predictions.append("Offensive")
-
-            # Make predictions using Model B
-            prediction_b = loaded_model_b.predict(X_unseen)
-            
-            # Check if the tweet is targeted
-            if prediction_b == 0:
-                predictions.append("Untargeted")
-            else:
-                predictions.append("Targeted")
-
-                # Make predictions using Model C
-                prediction_c = loaded_model_c.predict(X_unseen)
-                if prediction_c == 0:
-                    predictions.append('Group') 
-                elif prediction_c == 1:
-                    predictions.append('Individual') 
-                else:
-                    predictions.append('Others') 
-
-        return predictions
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error occurred while making predictions: {e}")
-
+#defining end point
 @app.post("/predict")
-def predict(tweet_request: TweetRequest):
+# tweetrequest object is taken as input representing the tweet to be predicted
+async def predict(tweet_request: TweetRequest):
+    #calling the load models fucntion to load the trained models
     loaded_model_a, loaded_model_b, loaded_model_c, tfidf_vectorizer = load_models()
+    #generating predictions
     predictions = make_predictions(tweet_request.tweet, tfidf_vectorizer, loaded_model_a, loaded_model_b, loaded_model_c)
-    return {"predictions": predictions}
+    return {"The tweet is" : predictions}
+
